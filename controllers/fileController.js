7902636b398949dc0b441d3400cbfcbd7f66f0b5
BOCbMOU <bocbou@gmail.com>
2019-08-11 20:20:54 +0300

    Update 5

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -2,31 +2,25 @@ import AppError from '../errors/AppError';
 
 const saveFile = async (req, res, next) => {
   try {
-    const { image } = req.files;
-    if(!image) {
+    const { file } = req.files;
+    if (!file) {
       return res.status(404).send('Didnt get file');
     }
     const now = new Date();
 
-    let ext = image.name.split('.');
+    let ext = file.name.split('.');
     if (!Array.isArray(ext) && ext.length > 1) {
-      ext = image.mimeType.split('/');
+      ext = file.mimeType.split('/');
     }
 
-    const newFileName = `img_${now.getDay()}-${now.getMonth()}-${now.getFullYear()}-${now.getMilliseconds()}.${ext.pop()}`;
-    image.mv(`${__dirname}/../public/images/${newFileName}`);
+    const newFileName =
+      `img_${now.getDate()}-${`0${now.getMonth()}`.slice(-2)}-${now.getFullYear()}_` +
+      `${now.getTime()}.${ext.pop()}`;
+    file.mv(`${__dirname}/../public/files/${newFileName}`);
     res.status(201).send('ok');
   } catch (err) {
     next(new AppError(err.message, 400));
   }
 };
 
-const loadFile = async (req, res, next) => {
-  try {
-    res.send('ok');
-  } catch (err) {
-    next(new AppError(err.message, 400));
-  }
-};
-
-export { saveFile, loadFile };
+export { saveFile };
