a55deda31ee9d6885012a641ec9dba5119661fc4
BOCbMOU <bocbou@gmail.com>
2019-08-11 21:19:22 +0300

    Update 5

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -3,20 +3,23 @@ import AppError from '../errors/AppError';
 const saveFile = async (req, res, next) => {
   try {
     const { file } = req.files;
+    const { body } = req;
     if (!file) {
       return res.status(404).send('Didnt get file');
     }
     const now = new Date();
 
     let ext = file.name.split('.');
-    if (!Array.isArray(ext) && ext.length > 1) {
-      ext = file.mimeType.split('/');
+    const type = file.mimetype.split('/');
+    if (!Array.isArray(ext)) {
+      ext = type;
     }
 
     const newFileName =
-      `img_${`0${now.getDate()}`.slice(-2)}-${`0${now.getMonth()}`.slice(-2)}-${now.getFullYear()}_` +
-      `${now.getTime()}.${ext.pop()}`;
-    file.mv(`${__dirname}/../public/files/${newFileName}`);
+      `${type[0]}_${`0${now.getDate()}`.slice(-2)}_${body.entityId}-${`0${now.getMonth()}`.slice(-2)}-` +
+      `${now.getFullYear()}_${now.getTime()}`;
+    file.mv(`${__dirname}/../public/${body.entity}/${newFileName}.${ext.pop()}`);
+
     res.status(201).send('ok');
   } catch (err) {
     next(new AppError(err.message, 400));
